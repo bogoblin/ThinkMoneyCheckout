71dea379474d0ed51e26ee809e1ef557be9f9efd
Bobby McCann <bobby@bogoblin.com>
2024-05-03 15:46:05 +0100

    Implement Deal.Apply and Deal.AppliesTo

--- cart.go
+++ cart.go
@@ -6,11 +6,35 @@ type Deal struct {
 }
 
 func (deal *Deal) Apply(total int, cart map[string]int) (int, map[string]int) {
-	return total, cart
+	// We return a new cart instead of modifying the original one
+	cartAfterDealApplied := make(map[string]int)
+	for sku, quantity := range cart {
+		quantityRequired, inDeal := deal.skus[sku]
+		if !inDeal {
+			cartAfterDealApplied[sku] = quantity
+		} else {
+			cartAfterDealApplied[sku] = quantity - quantityRequired
+		}
+	}
+
+	return total + deal.price, cartAfterDealApplied
 }
 
 func (deal *Deal) AppliesTo(cart map[string]int) bool {
-	return false
+	// Check if all SKUs in the deal are in the cart
+	for sku, quantityRequired := range deal.skus {
+		// Deals shouldn't have negative or zero quantities but let's check anyway,
+		// since it would cause an infinite loop if somehow there was.
+		if quantityRequired <= 0 {
+			return false
+		}
+		numInCart, ok := cart[sku]
+		if !ok || numInCart < quantityRequired {
+			return false
+		}
+	}
+	// If we get here, all SKUs in the deal are in the cart
+	return true
 }
 
 type SKU struct {
