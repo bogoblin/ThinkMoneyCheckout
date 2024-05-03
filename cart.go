d0014221d0f29c66c6851532e44103d301675579
Bobby McCann <bobby@bogoblin.com>
2024-05-03 15:18:06 +0100

    Add logic to sum the prices and apply deals
    The deals logic is currently stubbed out

--- cart.go
+++ cart.go
@@ -5,15 +5,35 @@ type Deal struct {
 	price int            // Price after discount
 }
 
+func (deal *Deal) Apply(total int, cart map[string]int) (int, map[string]int) {
+	return total, cart
+}
+
+func (deal *Deal) AppliesTo(cart map[string]int) bool {
+	return false
+}
+
 type SKU struct {
 	name  string
 	price int
 }
 
-func CalculateTotal(skusInCart map[SKU]int, deals []Deal) int {
+func CalculateTotal(cart map[string]int, unitPriceMap map[string]int, deals []Deal) int {
 	total := 0
-	for sku, quantity := range skusInCart {
-		total += sku.price * quantity
+
+	// Apply deals
+	for _, deal := range deals {
+		// A deal can apply multiple times:
+		for deal.AppliesTo(cart) {
+			total, cart = deal.Apply(total, cart)
+		}
+	}
+	for sku, quantity := range cart {
+		price, ok := unitPriceMap[sku]
+		if !ok {
+			panic("No price for SKU")
+		}
+		total += price * quantity
 	}
 	return total
 }
--- cart_test.go
+++ cart_test.go
@@ -4,8 +4,15 @@ import "testing"
 
 func TestCalculateTotal(t *testing.T) {
 	type args struct {
-		skusInCart map[SKU]int
-		deals      []Deal
+		skusInCart   map[string]int
+		unitPriceMap map[string]int
+		deals        []Deal
+	}
+	exampleUnitPriceMap := map[string]int{
+		"A": 50,
+		"B": 30,
+		"C": 20,
+		"D": 15,
 	}
 	exampleDeals := []Deal{
 		{
@@ -29,53 +36,57 @@ func TestCalculateTotal(t *testing.T) {
 		{
 			"Returns 0 if no items in cart",
 			args{
-				skusInCart: map[SKU]int{},
-				deals:      []Deal{},
+				skusInCart:   map[string]int{},
+				unitPriceMap: exampleUnitPriceMap,
+				deals:        []Deal{},
 			},
 			0,
 		},
 		{
 			"No deals apply",
 			args{
-				skusInCart: map[SKU]int{
-					{name: "A", price: 50}: 2,
-					{name: "B", price: 30}: 1,
-					{name: "C", price: 20}: 3,
-					{name: "D", price: 15}: 2,
+				skusInCart: map[string]int{
+					"A": 2,
+					"B": 1,
+					"C": 3,
+					"D": 2,
 				},
-				deals: exampleDeals,
+				unitPriceMap: exampleUnitPriceMap,
+				deals:        exampleDeals,
 			},
 			2*50 + 30 + 3*20 + 2*15,
 		},
 		{
 			"Deals from example each apply once",
 			args{
-				skusInCart: map[SKU]int{
-					{name: "A", price: 50}: 3,
-					{name: "B", price: 30}: 2,
-					{name: "C", price: 20}: 1,
-					{name: "D", price: 15}: 1,
+				skusInCart: map[string]int{
+					"A": 3,
+					"B": 2,
+					"C": 1,
+					"D": 1,
 				},
-				deals: exampleDeals,
+				unitPriceMap: exampleUnitPriceMap,
+				deals:        exampleDeals,
 			},
 			130 + 45 + 20 + 15,
 		},
 		{
 			"Deals from example apply multiple times",
 			args{
-				skusInCart: map[SKU]int{
-					{name: "A", price: 50}: 6,
-					{name: "B", price: 30}: 8,
-					{name: "C", price: 20}: 1,
+				skusInCart: map[string]int{
+					"A": 6,
+					"B": 8,
+					"C": 1,
 				},
-				deals: exampleDeals,
+				unitPriceMap: exampleUnitPriceMap,
+				deals:        exampleDeals,
 			},
 			2*130 + 4*45 + 20,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := CalculateTotal(tt.args.skusInCart, tt.args.deals); got != tt.want {
+			if got := CalculateTotal(tt.args.skusInCart, tt.args.unitPriceMap, tt.args.deals); got != tt.want {
 				t.Errorf("CalculateTotal() = %v, want %v", got, tt.want)
 			}
 		})
